8896ce73d479a1871de60192aae5ae88f804dfb4
Angel Travieso <angeltraviesoc@gmail.com>
2022-08-23 13:54:05 -0400

    NoteView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,7 +2,7 @@ import { IconButton } from '@mui/material';
 import { AddOutlined } from '@mui/icons-material';
 
 import { JournalLayout } from '../layout/JournalLayout';
-import { NothingSelectedView } from '../views';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
@@ -10,8 +10,8 @@ export const JournalPage = () => {
       
       {/* <Typography>Sint id officia amet velit do aliqua aliqua est ea velit minim voluptate duis laboris. Esse esse consectetur ullamco excepteur ullamco amet. Mollit est nostrud nisi irure magna dolor eiusmod aliquip aliqua nostrud incididunt enim. Velit ipsum laborum Lorem anim laboris aute ullamco ipsum do adipisicing irure.</Typography> */}
 
-      <NothingSelectedView />
-      {/* <NoteView /> */}
+      {/* <NothingSelectedView /> */}
+      <NoteView />
 
 
     </JournalLayout>
---
+++ src/journal/views/NoteView.jsx
@@ -0,0 +1,54 @@
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import { SaveOutlined } from '@mui/icons-material';
+import { ImageGallery } from "../components";
+
+const date = new Date().toLocaleDateString('es-VE');
+
+export const NoteView = () => {
+  return (
+    <Grid
+        container
+        direction='row'
+        justifyContent='space-between'
+        alignItems='center'
+        sx={{ mb: 1 }}
+    >
+        <Grid item>
+            <Typography fontSize={ 39 } fontWeight='light'>{  date }</Typography>
+        </Grid>
+
+        <Grid item>
+            <Button color='primary' sx={{ padding: 2 }}>
+                <SaveOutlined sx={{ fontSize: 30, mr: 1 }} /> 
+                Guardar
+            </Button>
+        </Grid>
+
+        <Grid container>
+            <TextField
+                type='text'
+                variant='filled'
+                fullWidth
+                placeholder="Ingrese un título"
+                label='Título'
+                sx={{ border: 'none', mb: 1 }}
+            />
+        </Grid>
+
+        <Grid container>
+            <TextField
+                type='text'
+                variant='filled'
+                fullWidth
+                multiline
+                placeholder="¿Qué sucedió en el día de hoy?"
+                minRows={ 5 }
+            />
+        </Grid>
+
+        {/* Galería de Imágenes */}
+        <ImageGallery />
+
+    </Grid>
+  )
+}
