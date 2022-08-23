3b85a8e3865b624cf760793a409a034939cf86a0
Angel Travieso <angeltraviesoc@gmail.com>
2022-08-23 11:53:53 -0400

    JournalLayout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,11 @@
 import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere unde culpa iure corrupti doloribus! Laboriosam quidem molestiae modi blanditiis omnis sunt, cupiditate, eligendi possimus voluptatum labore rem similique neque et.</Typography>
+    </JournalLayout>
   )
 }
