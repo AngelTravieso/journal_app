8efeb125e3cfa76d527fb4fbb1059604c5b7b915
Angel Travieso <angeltraviesoc@gmail.com>
2022-09-19 11:57:40 -0400

    mantener el estado de la autenticacion al recargar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,36 @@
-import { useSelector } from "react-redux";
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
 
+import { Navigate, Route, Routes } from "react-router-dom";
+import { FirebaseAuth } from "../firebase/config";
+
+import { login, logout } from "../store/auth";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
+
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
 import { CheckingAuth } from "../ui/components/CheckingAuth";
 
 export const AppRouter = () => {
 
   const { status } = useSelector( state => state.auth );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+
+    // Cuando el estado de la autenticacion cambia
+    onAuthStateChanged( FirebaseAuth, async( user ) => {
+      
+      if( !user ) return dispatch( logout() );
+
+      const { uid, displayName, email, photoURL } = user;
+
+      dispatch( login( { uid, displayName, email, photoURL }) );
+
+    });
+
+
+  }, []);
 
   if( status === 'checking') {
     return <CheckingAuth />
@@ -15,11 +38,20 @@ export const AppRouter = () => {
 
   return (
     <Routes>
+
+      {
+        (status === 'authenticated')
+        ? <Route path="/*" element={ <JournalRoutes /> } />
+        : <Route path="/auth/*" element={ <AuthRoutes /> } />
+      }
+
+      <Route path='/*' element={ <Navigate to='/auth/login' /> } />
+
         {/* Login y Registro */}
-        <Route path="/auth/*" element={ <AuthRoutes /> } />
+        {/* <Route path="/auth/*" element={ <AuthRoutes /> } /> */}
 
         {/* JournalApp */}
-        <Route path="/*" element={ <JournalRoutes /> } />
+        // <Route path="/*" element={ <JournalRoutes /> } />
 
     </Routes>
   )
