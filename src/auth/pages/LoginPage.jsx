c2979777120af30cf9d9764c9480ea20e87ee200
Angel Travieso <angeltraviesoc@gmail.com>
2022-09-19 10:58:29 -0400

    mostrar errores en login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,7 +2,7 @@ import { useMemo } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
-import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Alert, Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { DisplaySettings, Google } from "@mui/icons-material";
 
 import { AuthLayout } from '../layout/AuthLayout';
@@ -28,7 +28,7 @@ export const LoginPage = () => {
   const dispatch = useDispatch();
   const [formSubmitted, setFormSubmitted] = useState(false);
   
-  const { status } = useSelector( state => state.auth );
+  const { status, errorMessage } = useSelector( state => state.auth );
 
   const { 
     formState, email, password, onInputChange,
@@ -99,7 +99,7 @@ export const LoginPage = () => {
                   variant='contained'
                   fullWidth
                   >
-                  Login
+                  Login 
                 </Button>
               </Grid>
 
@@ -114,6 +114,13 @@ export const LoginPage = () => {
                   <Google />
                   <Typography sx={{ ml: 1 }}>Google</Typography>
                 </Button>
+              </Grid>
+
+              <Grid
+                item
+                xs={ 12 }
+              >
+                <Alert severity='error'>{ errorMessage }</Alert>
               </Grid>
             
             </Grid>
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -25,7 +25,7 @@ export const RegisterPage = () => {
   const [formSubmitted, setFormSubmitted] = useState(false);
 
   const { status, errorMessage } = useSelector( state => state.auth );
-  const isCheckingAuthentication = useMemo( () => status === 'checking', [ status]);
+  const isCheckingAuthentication = useMemo( () => status === 'checking', [status]);
 
   const { 
     formState, displayName, email, password, onInputChange,
